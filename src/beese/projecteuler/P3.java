eac622c6cbf36b731bf6fc03b937ac0923c9fc4d
Kristin Beese <beese@purdue.edu>
2015-02-21 14:13:03 -0500

    Renamed package

--- src/beese/projecteuler/P3.java
+++ src/beese/projecteuler/P3.java
@@ -1,8 +1,8 @@
-package cs182;
+package beese.projecteuler;
 
 import java.util.ArrayList;
 
-public class ProjectEuler3 {
+public class P3 {
 	
 	//Project Euler Problem 3: Largest Prime Factor
 	//The prime factors of 13195 are 5, 7, 13 and 29.
